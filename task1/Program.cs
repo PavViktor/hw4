25a0dafe2e88e49bf69fe42892feb599b5127a5a
vsf.viktor <vsf.viktor@gmail.com>
2022-12-07 19:37:49 +0300

    homework complete

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,3 +3,14 @@
 Задание должно быть выполнено в методе. 
 В методе не должно быть вывода в консоль. Не использовать Math.Pow() */
 
+int pow(int num, int times)
+{
+    int result = 1;
+
+    for (int i = 0; i < times; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
+
--- task3/Program.cs
+++ task3/Program.cs
@@ -2,3 +2,31 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33] */
 
+
+int[] fillArray(int size)
+
+{
+    int[] numbersArray = new int[size];
+    int i = 0;
+    do
+    {
+        Console.WriteLine("Input the number: ");
+        bool isNumber = int.TryParse(Console.ReadLine(), out int num);
+        
+
+        if (!isNumber)
+        {
+            Console.WriteLine("Not a number! Try again");
+            continue;
+        }
+
+        numbersArray[i] = num;
+        i++;
+
+    } while (i < size);
+
+    return numbersArray;
+}
+
+Console.WriteLine("[" + String.Join(", ", fillArray(5)) + "]");
+Console.WriteLine("[" + String.Join(", ", fillArray(3)) + "]");
